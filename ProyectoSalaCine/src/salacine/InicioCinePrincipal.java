7e37983e687615ba39bb99eeb4e8e5b0d203369b
Daniel Garcia Merino <danielgm2626@gmail.com>
2016-01-21 21:25:11 +0100

    Commit de prueba reemplazando package

---
+++ ProyectoSalaCine/src/salacine/InicioCinePrincipal.java
@@ -0,0 +1,18 @@
+package salacine;
+
+
+
+public class InicioCinePrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Empezando hilos");
+		/*Thread hilo1 = new InicioCine();
+		Thread hilo2 = new InicioCine();
+		Thread hilo3 = new InicioCine();
+		System.out.println("Arrancando hilos");
+		hilo1.start();hilo2.start();hilo3.start();
+		System.out.println("Hilos arrancados");*/
+	}
+
+}
